accfa9007b50cdac1295d013741bde48704cc573
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-12 15:28:56 +0530

    setting up the module structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,10 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.material)
+
+//    implementation(libs.androidx.material3)
+//    implementation(libs.androidx.material3.android)
 
 
     testImplementation(libs.junit)
--- app/src/main/java/com/fahim/calorietracker/ui/theme/Type.kt
+++ app/src/main/java/com/fahim/calorietracker/ui/theme/Type.kt
@@ -1,49 +1,48 @@
 package com.fahim.calorietracker.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
-    labelLarge = TextStyle(
+    button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
         fontSize = 20.sp,
     ),
-    labelMedium = TextStyle(
+    caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
-    displayLarge = TextStyle(
+    h1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 30.sp
     ),
-    displayMedium = TextStyle(
+    h2 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 26.sp
     ),
-    displaySmall = TextStyle(
+    h3 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp
     ),
-    headlineLarge = TextStyle(
+    h4 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 18.sp
     ),
-    headlineMedium = TextStyle(
+    h5 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
